cb5e43bbb161abe8299226f429af50ac8c247f83
liz rush <liz@lizmrush.com>
2014-12-03 20:07:55 -0800

    Create OrderedDictionary

---
+++ FlickrSearch/OrderedDictionary.swift
@@ -0,0 +1,47 @@
+//
+//  OrderedDictionary.swift
+//  FlickrSearch
+//
+//  Created by liz on 3/12/14.
+//  Copyright (c) 2014 Razeware. All rights reserved.
+//
+
+import Foundation
+
+struct OrderedDictionary<KeyType: Hashable, ValueType> {
+    typealias ArrayType = [KeyType]
+    typealias DictionaryType = [KeyType: ValueType]
+    
+    var array = ArrayType()
+    var dictionary = DictionaryType()
+    
+    mutating func insert(value: ValueType,
+        forKey key: KeyType,
+        atIndex index: Int) -> ValueType?
+    {
+        var adjustedIndex = index
+        
+        let existingValue = self.dictionary[key]
+        if existingValue != nil {
+            let existingIndex = find(self.array, key)!
+            
+            if existingIndex < index {
+                adjustedIndex--
+            }
+            self.array.removeAtIndex(existingIndex)
+        }
+        self.array.insert(key, atIndex: adjustedIndex)
+        self.dictionary[key] = value
+        
+        return existingValue
+    }
+    
+    mutating func removeAtIndex(index: Int) -> (KeyType, ValueType)
+    {
+        precondition(index < self.array.count, "Index out-of-bounds")
+        let key = self.array.removeAtIndex(index)
+        let value = self.dictionary.removeValueForKey(key)!
+        return (key, value)
+    }
+    
+}
